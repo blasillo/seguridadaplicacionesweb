dff86b69c34a3e688ec3d16d43d18e114f0c1b01
blas <btorregrosa@infor.uva.es>
2024-07-16 14:27:50 +0200

    web login - servicio

--- web-login/web-login/src/main/java/es/jcyl/formacion/seguridadweb/weblogin/datos/entidades/RolUsuario.java
+++ web-login/web-login/src/main/java/es/jcyl/formacion/seguridadweb/weblogin/datos/entidades/RolUsuario.java
@@ -1,4 +1,13 @@
 package es.jcyl.formacion.seguridadweb.weblogin.datos.entidades;
 
 public interface RolUsuario {
+
+    Long getId();
+    void setId(Long id);
+
+    Long getUserId();
+    void setUserId(Long userId);
+
+    String getRol();
+    void setRol(String r);
 }
--- web-login/web-login/src/main/java/es/jcyl/formacion/seguridadweb/weblogin/servicios/UsuarioServicio.java
+++ web-login/web-login/src/main/java/es/jcyl/formacion/seguridadweb/weblogin/servicios/UsuarioServicio.java
@@ -1,6 +1,18 @@
 package es.jcyl.formacion.seguridadweb.weblogin.servicios;
 
+import es.jcyl.formacion.seguridadweb.weblogin.datos.entidades.Usuario;
+
+import java.util.Optional;
+
 public interface UsuarioServicio {
 
+
+    Optional<Usuario> encontrarUsuarioPorNombreClave (String username, String password);
+
+    Optional<Usuario> getUsuarioInfo(String userId);
+
+    default Optional<Usuario> obtenerUsuarioInfoPorNombre (String username) {
+        return Optional.empty();
+    }
 
 }
